51e6dda7cd27c406d15874da584c486aa7b6997b
Amjad Ali (MJ) <amjad.hamdi.ali@gmail.com>
2018-08-18 18:58:55 -0500

    Instantiated Node Object from class

--- unival.py
+++ unival.py
@@ -1,5 +1,6 @@
 print('Unival Tree!')
 
+# Tree class to instantiate Node Tree object
 class Tree:
 	def __init__(self,value):
 		self.value = value
@@ -8,8 +9,16 @@ class Tree:
 
 		self.right = None
 
-
+# Instance of Tree Class (Our Node)
 root = Tree(5)
+root.left = Tree(1)
+root.right = Tree(5)
+root.left.left = Tree(5)
+root.left.right = Tree(5)
+root.right.right = Tree(5)
 
-print(root.value)		
+print(root.value)	
+
+
+
 
