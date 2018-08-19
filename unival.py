139b9ccff40ebc66809c7a277507db2bbad8b924
Amjad Ali (MJ) <amjad.hamdi.ali@gmail.com>
2018-08-18 19:39:56 -0500

    if the left root does not equal right root, not unival

--- unival.py
+++ unival.py
@@ -21,18 +21,19 @@ def count_univals (root):
 	# If the root does not exist
 	if (root == None):
 		return(0, True)
-
+	# Recursively check every branch and unpack return value
 	left_count, left_unival = count_univals(root.left)
-	
 	right_count, right_unival = count_univals(root.right)
 
-
+	# Assume is unival until one of the following coditions tells us otherwise
 	is_unival = True
+	# if the left root does not equal right root, not unival
+	if (not left_unival or not right_unival ):
+		is_unival = False
 	# If the left root exist and does not equal root, then not unival
 	if (root.left and root.left.value != root.value):
 		is_unival = False
 	# If the right root exist and does not equal root, then not unival
-
 	if (root.right and root.right.value != root.value):
 		is_unival = False
 
